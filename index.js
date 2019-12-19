2c4f6f1a2575d261315062ef848a92bbc7573278
Christene Bennett <cbennett9396@FS1437MBPRO13.strongmind.com>
2019-12-19 15:13:00 -0700

    add scripts

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const server = require("./server");
 
-const PORT = 9090;
+const PORT =  process.env.PORT || 9090;
 
 server.listen(PORT, () => {
   console.log(`The server is listening on port ${PORT}`);
